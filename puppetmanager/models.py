1d19828c170797c97cf293ce4ea2ec35052b1395
Daniel Mateos <daniel@mateos.cc>
2019-11-02 05:18:00 +1100

    basic external_classify + tests

--- puppetmanager/models.py
+++ puppetmanager/models.py
@@ -1,3 +1,4 @@
+import yaml
 from django.db import models
 
 
@@ -18,9 +19,13 @@ class Node(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=128, null=False)
 
-    classifications = models.ManyToManyField(Configuration)
+    classifications = models.ManyToManyField(Classification)
 
     def external_classify(self):
         base_data = {"classes": {}, "parameters": {}, "environment": "production"}
 
-        return base_data
+        for classification in self.classifications.all():
+            for config in classification.configurations.all():
+                base_data["classes"][config.name] = {}
+
+        return yaml.dump(base_data, default_flow_style=False, explicit_start=True)
--- puppetmanager/tests/test_puppetmanager_models.py
+++ puppetmanager/tests/test_puppetmanager_models.py
@@ -1,11 +1,46 @@
-from puppetmanager.models import Node
+import pytest
+import yaml
+from puppetmanager.models import Node, Classification, Configuration
 
 
 # Node
+@pytest.mark.django_db
 def test_external_classifier_format():
     expected_data = {"classes": {}, "parameters": {}, "environment": "production"}
+    expected_data = yaml.dump(
+        expected_data, default_flow_style=False, explicit_start=True
+    )
 
     node = Node(name="test node")
+    node.save()
+
+    yaml_data = node.external_classify()
+
+    assert yaml_data == expected_data
+
+
+@pytest.mark.django_db
+def test_external_classifier_lists_configurations_in_classification():
+    expected_data = {
+        "classes": {"testconfiguration": {}},
+        "parameters": {},
+        "environment": "production",
+    }
+
+    expected_data = yaml.dump(
+        expected_data, default_flow_style=False, explicit_start=True
+    )
+
+    configuration = Configuration(name="testconfiguration")
+    configuration.save()
+
+    classification = Classification()
+    classification.save()
+    classification.configurations.add(configuration)
+
+    node = Node(name="test node")
+    node.save()
+    node.classifications.add(classification)
 
     yaml_data = node.external_classify()
 
