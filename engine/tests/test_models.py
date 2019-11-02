effb851c6a23b049d4ff58606112a5e86b4b513e
Daniel Mateos <daniel@mateos.cc>
2019-11-03 01:35:18 +1100

    start on command tests in puppetmanager

--- engine/tests/test_models.py
+++ engine/tests/test_models.py
@@ -1,5 +1,4 @@
 import pytest
-
 from engine.models import Account, AppConfiguration
 
 
---
+++ puppetmanager/tests/test_puppetmanager_commands.py
@@ -0,0 +1,17 @@
+# import pytest
+# from puppetmanager.management.commands import Command
+
+
+# NodeClassify
+
+
+def test_nodeclassify_accepts_correct_parameter():
+    pass
+
+
+def test_nodeclassify_command_returns_for_valid_name():
+    pass
+
+
+def test_nodeclassify_creates_new_node_on_missing_name():
+    pass
