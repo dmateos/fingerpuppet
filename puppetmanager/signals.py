9b733c08c02ae22fddde0f74dbdfb4f5ab124f74
Daniel Mateos <daniel@mateos.cc>
2019-11-09 22:06:02 +1100

    add signal to auto bake to file config changes if env var is set

---
+++ puppetmanager/signals.py
@@ -0,0 +1,11 @@
+import os
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from .models import Configuration
+
+
+@receiver(post_save, sender=Configuration, dispatch_uid="config_save_update_files")
+def configuration_update(sender, instance, **kwargs):
+    module_path = os.environ.get("PUPPET_MODULE_PATH", False)
+    if module_path:
+        instance.bake_to_file(module_path)
